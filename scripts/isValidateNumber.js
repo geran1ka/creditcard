8952671cd0715b9a03fc3a1263ea30211c1df565
Geran1ka <roman.khoruzhy@gmail.com>
2023-10-25 15:57:52 +0300

    test

--- scripts/isValidateNumber.js
+++ scripts/isValidateNumber.js
@@ -8,24 +8,20 @@
 количеством цифр и со слишком большим количеством цифр (например, 18).
 */
 const isValidateNumberCard = num => {
-  console.log('num: ', num);
-  console.log('num: ', num.length);
-
-  
   const test = num.toString();
 
   if (!/^\d+$/.test(test)) {
-    console.log('Не цифра');
+    // console.log('Не цифра');
     return false;
   }
 
   if (test.length < 16) {
-    console.log('Длина меньше 16');
+    // console.log('Длина меньше 16');
     return false;
   }
 
   if (test.length > 16) {
-    console.log('Длина больше 16');
+    // console.log('Длина больше 16');
     return false;
   }
   return true;
